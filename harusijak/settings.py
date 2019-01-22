c11adad422823f18b407b5d97674a77630649a1a
Sunghwan Park <shoark7@gmail.com>
2019-01-22 14:15:22 +0900

    Change static location

--- harusijak/settings.py
+++ harusijak/settings.py
@@ -174,7 +174,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 AWS_STATIC_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'harusijak.storage_backends.StaticStorage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
 
 AWS_MEDIA_LOCATION = 'media'
