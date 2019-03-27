820e08306bb87249a7336b2b8bb1e76e0ae1b5b2
Sunghwan Park <shoark7@gmail.com>
2019-03-27 14:46:23 +0900

    Fix typo

--- harusijak/settings.py
+++ harusijak/settings.py
@@ -209,4 +209,4 @@ REST_FRAMEWORK = {
 
 import dj_database_url
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
