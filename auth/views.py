2d91bce2f6cbb31c9b9f892d6976376009fa7ce5
Sunghwan Park <shoark7@gmail.com>
2019-04-01 17:04:14 +0900

    Correct login api

--- auth/views.py
+++ auth/views.py
@@ -16,10 +16,10 @@ from rest_framework.status import (
 @api_view(["POST"])
 @permission_classes((AllowAny,))
 def login(request):
-    identifier = request.data.get("identifier").strip()
-    password = request.data.get("password").strip()
+    identifier = request.data.get("identifier", None)
+    password = request.data.get("password", None)
     if identifier is None or password is None:
-        return Response({'error': 'Please provide both identifier and password'},
+        return Response({'error': 'Please provide correct identifier and password'},
                         status=HTTP_400_BAD_REQUEST)
     user = authenticate(identifier=identifier, password=password)
     if user is None:
