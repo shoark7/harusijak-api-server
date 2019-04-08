76b4acfa2406a6e1d13999acd77867fc07d604c1
Sunghwan Park <shoark7@gmail.com>
2019-04-08 11:49:29 +0900

    Alter Unique Validation messages

--- poet/serializers.py
+++ poet/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework import validators
 
 from .models import Poet
 from poem.models import Poem
@@ -11,12 +12,30 @@ class SimplePoetSerializer(serializers.ModelSerializer):
         fields = ('id', 'nickname',)
 
 
-class PoetSerializer(serializers.HyperlinkedModelSerializer):
+class BasePoetSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Poet
+        fields = ['url', 'pk', 'identifier', 'password', 'nickname',
+                  'image', 'description',]
+
+class PoetSerializer(BasePoetSerializer):
     poems_all_count = serializers.SerializerMethodField()
     likes_count = serializers.SerializerMethodField()
     poems_displayed_count = serializers.SerializerMethodField()
     subscribed_count = serializers.SerializerMethodField()
     keeped_count = serializers.SerializerMethodField()
+
+    def __init__(self, *args, **kwargs):
+        super(PoetSerializer, self).__init__(*args, **kwargs)
+        # Find UniqueValidator and set custom message
+        unique_check_fields = ['identifier', 'nickname', 'pk']
+        unique_check_messages = ['해당 ID는 이미 존재합니다.', '해당 필명은 이미 존재합니다.',
+                                '개인의 고유 PK는 변경할 수 없습니다.',]
+
+        for field, message in zip(unique_check_fields, unique_check_messages):
+            for validator in self.fields[field].validators:
+                if isinstance(validator, validators.UniqueValidator):
+                    validator.message = (message)
 
     def get_poems_all_count(self, obj):
         return Poem.objects.filter(writer=obj).count()
@@ -36,25 +55,11 @@ class PoetSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Poet
-        # First block: model field
-        # Second block: custom field
         fields = ['url', 'pk', 'identifier', 'password', 'nickname',
                   'image', 'description',
 
                   'poems_all_count', 'poems_displayed_count', 'subscribed_count',
                   'keeped_count', 'likes_count', ]
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
